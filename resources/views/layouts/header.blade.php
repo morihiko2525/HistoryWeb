6701047b687fac4d9576e61bd4feec2c464956ce
Yuki Mori <morihiko223@gmail.com>
2021-04-30 02:48:11 +0000

    small

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -23,7 +23,7 @@
                     <ul class="navbar-nav">
                         {{-- メッセージ作成ページへのリンク --}}
                         <li class="nav-item"><a href = "{{ Route('signup.get') }}">ログイン</a></li>
-                        <li class="nav-item"><a href = "{{ Route('signup.get') }}">アカウント作成</a></li>
+                        <li class="nav-item"><a href = "{{ Route('login') }}">アカウント作成</a></li>
 
                         @if (Auth::check())
                             {{-- ユーザ一覧ページへのリンク --}}
