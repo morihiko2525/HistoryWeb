cf395e80d5a99152dad182ca000ebda2e77d8c8e
Yuki Mori <morihiko223@gmail.com>
2021-04-30 09:03:29 +0000

    トップページに孫正義とダイエーのダミーデーターを配置

--- resources/views/history_view.blade.php
+++ resources/views/history_view.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.header')
 
 @section('content')
-
+<h1>只今絶賛開発中。</h1>
+<h1>ここに年表画面を作成します</h1>
+<h1>フィードバックをお待ちしております。</h1>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,10 +28,10 @@
         <div class = history-table-content>
             <div class = linkBox>
                 <div class = ht-sumnail>
-                    <img src = "https://riverwalk.co.jp/app/wp-content/uploads/2019/03/20190318-saizeriya_logo.jpg" width = 200px height = 200px>
+                    <img src = "https://pbs.twimg.com/profile_images/2579542348/l2b371d8r57tu6m4wu7i_400x400.jpeg" width = 200px height = 200px>
                 </div>
             <a href="https://www.google.co.jp"></a>
-            <p class = ht-title>サイゼリヤ年表</p>
+            <p class = ht-title>孫正義の人生</p>
             <p>ここは年表説明欄です。サイゼリヤの年表を作りたいと思っています。</p>
                 </div>
         </div>
@@ -39,10 +39,10 @@
         <div class = history-table-content>
             <div class = linkBox>
                 <div class = ht-sumnail>
-                    <img src = "https://riverwalk.co.jp/app/wp-content/uploads/2019/03/20190318-saizeriya_logo.jpg" width = 200px height = 200px>
+                    <img src = "https://pbs.twimg.com/profile_images/904746258052423680/WTxEMdkZ_400x400.jpg" width = 200px height = 200px>
                 </div>
             <a href="https://www.google.co.jp"></a>
-            <p class = ht-title>サイゼリヤ年表</p>
+            <p class = ht-title>ダイエーの大躍進と破産まで</p>
             <p>ここは年表説明欄です。サイゼリヤの年表を作りたいと思っています。</p>
                 </div>
         </div>
