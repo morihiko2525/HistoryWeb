99c955dd36937b5d9bad0448577c119acf3e35c2
Yuki Mori <morihiko223@gmail.com>
2021-04-30 02:23:14 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.header')
+
+@section('content')
+
+    <div class = top-img>
+        <p class = title>Webで年表作成</p>
+    </div>
+    
+    
+    <div class = "history-table-feature">
+    <h1>人気の年表</h1>
+        <div class = history-table-content>
+            <div class = linkBox>
+                <div class = ht-sumnail>
+                    <img src = "https://riverwalk.co.jp/app/wp-content/uploads/2019/03/20190318-saizeriya_logo.jpg" width = 200px height = 200px>
+                </div>
+            <a href="https://www.google.co.jp"></a>
+            <p class = ht-title>サイゼリヤ年表</p>
+            <p>ここは年表説明欄です。サイゼリヤの年表を作りたいと思っています。</p>
+                </div>
+        </div>
+            
+        <div class = history-table-content>
+            <div class = linkBox>
+                <div class = ht-sumnail>
+                    <img src = "https://riverwalk.co.jp/app/wp-content/uploads/2019/03/20190318-saizeriya_logo.jpg" width = 200px height = 200px>
+                </div>
+            <a href="https://www.google.co.jp"></a>
+            <p class = ht-title>サイゼリヤ年表</p>
+            <p>ここは年表説明欄です。サイゼリヤの年表を作りたいと思っています。</p>
+                </div>
+        </div>
+        
+        <div class = history-table-content>
+            <div class = linkBox>
+                <div class = ht-sumnail>
+                    <img src = "https://riverwalk.co.jp/app/wp-content/uploads/2019/03/20190318-saizeriya_logo.jpg" width = 200px height = 200px>
+                </div>
+            <a href="https://www.google.co.jp"></a>
+            <p class = ht-title>サイゼリヤ年表</p>
+            <p>ここは年表説明欄です。サイゼリヤの年表を作りたいと思っています。</p>
+                </div>
+        </div>
+    </div>
+    
+    <div class = site-feature>
+    <p>特徴</p>    
+    </div>
+@endsection
