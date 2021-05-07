35d919168624759887a605a50c25818e14d39425
Yuki Mori <morihiko223@gmail.com>
2021-05-07 04:03:14 +0000

    年表テーブルにuser_idを入れる処理を実装

--- resources/views/create_history.blade.php
+++ resources/views/create_history.blade.php
@@ -17,7 +17,10 @@
           <div class="form-group">
             {!! Form::label('description', '説明') !!}
             {!! Form::text('description', null, ['class' => 'form-control']) !!}
+          </div>
+            {!! Form::hidden('user_id', Auth::user()->id )!!}
           <button type="submit" class="btn btn-primary">作成</button>
+          
         {!! Form::close() !!}
         <a href= "{{ action('EventController@index', 1) }}" type = submit class="button">作成する！</a>
 </div>
