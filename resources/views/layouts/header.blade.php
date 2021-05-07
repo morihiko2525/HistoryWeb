14115545b06e73f5f855ac977c18c9155c331b03
Yuki Mori <morihiko223@gmail.com>
2021-05-07 01:52:23 +0000

    ヘッダーを日本語化

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -48,10 +48,10 @@
                                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
                                 <ul class="dropdown-menu dropdown-menu-right">
                                     {{-- ユーザ詳細ページへのリンク --}}
-                                    <li class="dropdown-item"><a href="#">My profile</a></li>
+                                    <li class="dropdown-item"><a href="#">プロフィール</a></li>
                                     <li class="dropdown-divider"></li>
                                     {{-- ログアウトへのリンク --}}
-                                    <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                                    <li class="dropdown-item">{!! link_to_route('logout.get', 'ログアウト') !!}</li>
                                 </ul>
                             </li>
                         @else
