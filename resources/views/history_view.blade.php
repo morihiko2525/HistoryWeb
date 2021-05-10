b5a06d4937aaedf6033963915cb8920cfc702946
Yuki Mori <morihiko223@gmail.com>
2021-05-10 06:08:36 +0000

    history_viewからモーダルを削除

--- resources/views/history_view.blade.php
+++ resources/views/history_view.blade.php
@@ -29,63 +29,7 @@
 @endforeach
             
             
-            
-            
-<!-- モーダルエリアここから -->
-
-<section id="modalArea" class="modalArea">
-  <div id="modalBg" class="modalBg"></div>
-  <div class="modalWrapper">
-    <div class="modalContents">
-      <h1>イベントを追加</h1>
-      
-        {!! Form::model($events, ['route' => 'events.store']) !!}
-          <div class="form-group">
-            {!! Form::label('name', 'イベント名') !!}
-            {!! Form::text('name', null, ['class' => 'form-control']) !!}
-
-            <small id="emailHelp" class="form-text text-muted">出来事を入力</small>
-          </div>
-          <div class="form-group">
-            {!! Form::label('description', '説明') !!}
-            {!! Form::text('description', null, ['class' => 'form-control']) !!}
-
-          </div>
-          <div class="form-group">
-            {!! Form::label('year', '年') !!}
-            {!! Form::text('year', null, ['class' => 'form-control']) !!}
-
-          </div>
-          <div class="form-group">
-            {!! Form::label('month', '月') !!}
-            {!! Form::text('month', null, ['class' => 'form-control']) !!}
-
-          </div>
-          <div class="form-group">
-            {!! Form::label('day', '日') !!}
-            {!! Form::text('day', null, ['class' => 'form-control']) !!}
-
-          </div>
-          {!! Form::hidden('his_id', $his_id)!!}
-
-          <button type="submit" class="btn btn-primary">Submit</button>
-        {!! Form::close() !!}
-            </div>
-    <div id="closeModal" class="closeModal">
-      ×
-    </div>
-  </div>
-</section>
-<!-- モーダルエリアここまで -->
-
-<script>$(function () {
-  $('#openModal').click(function(){
-      $('#modalArea').fadeIn();
-  });
-  $('#closeModal , #modalBg').click(function(){
-    $('#modalArea').fadeOut();
-  });
-});</script>
+        
 <div id = footer>
   
 <div class="wrapper">
