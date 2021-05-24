8cb7b309678771945fc38a9fec0bd9b817885f37
morihiko <morihiko223@gmail.com>
2021-05-24 13:27:53 +0900

    ログイン画面と新規登録画面をBootstrapにした

--- resources/js/components/Login.js
+++ resources/js/components/Login.js
@@ -25,19 +25,18 @@ export default class Login extends React.Component {
         const is_guest = this.props.isGuest;
         
         if(!is_guest){
-            return(<Redirect push to="/home" />);
+            return(<Redirect push to="/userpage" />);
         }
         
         return (
-        <div className="flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-          <div className="max-w-md w-full space-y-8">
+        <div className="container">
+          <div className="dialog-window">
             <div>
               <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">アカウントにログイン</h2>
             </div>
-            <form className="mt-8 space-y-6" onSubmit={this.tryLogin}>
-              <div className="rounded-md shadow-sm -space-y-px">
-                <div>
-                  <label htmlFor="email-address" className="sr-only">
+            <form onSubmit={this.tryLogin}>
+                <div className="form-group">
+                  <label htmlFor="email-address">
                     メールアドレス
                   </label>
                   <input
@@ -45,14 +44,14 @@ export default class Login extends React.Component {
                     name="email"
                     type="email"
                     required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    className="form-control"
                     placeholder="Email address"
                     value={this.state.email}
                     onChange={e => this.setState({'email':e.target.value})}
                   />
                 </div>
-                <div>
-                  <label htmlFor="password" className="sr-only">
+                <div className="form-group">
+                  <label htmlFor="password">
                     パスワード
                   </label>
                   <input
@@ -60,17 +59,16 @@ export default class Login extends React.Component {
                     name="password"
                     type="password"
                     required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    className="form-control"
                     placeholder="Password"
                     onChange={e => this.setState({'password':e.target.value})}
                   />
                 </div>
-              </div>
     
               <div>
                 <button
                   type="submit"
-                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="btn btn-primary"
                 >
                   ログイン
                 </button>
--- resources/js/components/Signup.js
+++ resources/js/components/Signup.js
@@ -32,51 +32,51 @@ export default class Signup extends React.Component {
         }
         
         return (
-        <div className="flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-          <div className="max-w-md w-full space-y-8">
+          <div className="container">
+          <div className="dialog-window">
             <div>
               <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">アカウントを新規作成</h2>
             </div>
             <form className="mt-8 space-y-6" onSubmit={this.trySignup}>
-              <div className="rounded-md shadow-sm -space-y-px">
+            <div className="form-group">
                 <div>
-                  <label htmlFor="email-address" className="sr-only">
-                    Email address
+                  <label htmlFor="email-address">
+                    メールアドレス
                   </label>
                   <input
                     id="email-address"
                     name="email"
                     type="email"
                     required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    className="form-control"
                     placeholder="Email address"
                     onChange={e => this.setState({'email':e.target.value})}
                   />
                 </div>
                 <div>
-                  <label htmlFor="name" className="sr-only">
-                    Name
+                  <label htmlFor="name">
+                    名前
                   </label>
                   <input
                     id="name"
                     name="name"
                     type="text"
                     required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    className="form-control"
                     placeholder="Name"
                     onChange={e => this.setState({'name':e.target.value})}
                   />
                 </div>
                 <div>
-                  <label htmlFor="password" className="sr-only">
-                    Password
+                  <label htmlFor="password">
+                    パスワード
                   </label>
                   <input
                     id="password"
                     name="password"
                     type="password"
                     required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    className="form-control"
                     placeholder="Password"
                     onChange={e => this.setState({'password':e.target.value})}
                   />
@@ -86,9 +86,9 @@ export default class Signup extends React.Component {
               <div>
                 <button
                   type="submit"
-                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="btn btn-primary"
                 >
-                  Sign up
+                  登録
                 </button>
               </div>
             </form>
